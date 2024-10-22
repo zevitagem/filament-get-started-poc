7cf686c38e9a934e78572084337305951dc66485
José Victor da Silva <josevictor@ultrahaus.com>
2024-10-22 08:15:19 -0300

    first commit

---
+++ app/Models/Owner.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Patient;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Owner extends Model
+{
+    use HasFactory;
+
+    public function patients(): HasMany
+    {
+        return $this->hasMany(Patient::class);
+    }
+}
